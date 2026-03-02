645ab87e1217494ad6fb8852d44a23445044c4a6
Naura <abivandyafirdhan@gmail.com>
2026-03-02 21:14:39 +0700

    uptd

--- src/components/myExperience.jsx
+++ src/components/myExperience.jsx
@@ -12,7 +12,7 @@ export default function MyExperience() {
         },
         {
             title: "Experience",
-            count: "1 years"
+            count: "1yrs"
         },
     ]
 
@@ -22,7 +22,9 @@ export default function MyExperience() {
                 <div className='items-experience'>
                     <h2>{items.title}</h2>
                     <p>{items.count}</p>
+                    
                 </div>
+                
             ))}
         </section>
     )
